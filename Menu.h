740b81fae5610c309c51b2a863716231644d1a49
VyacheslavLit <kinglva@yandex.ru>
2023-02-16 18:01:41 +0300

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Admin.h"
+#include "Chat.h"
+#include "PerCor.h"
+
+/*
+	Зоголовок меню содержит объявление 3-х функций вызываемых базовой функцией.
+
+	administration - функция реалилующая администрирование.
+	chat - функция реализующая чат.
+	perCor - (от personal correspondence) реализующая личную переписку
+	regulation - описание правил пользования программой.
+*/
+
+void administration();
+void chat();
+void perCor();
+void regulation();
